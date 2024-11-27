6bf2a4691ff6466c6a3678878f8189e81b942297
Eden-hnnt <eden.henninot@gmail.com>
2024-11-27 11:16:01 +0100

    Revert "Develop"

--- models/intermediate/total_EU_death_rate_by_pathology.sql
+++ models/intermediate/total_EU_death_rate_by_pathology.sql
@@ -1,20 +1,9 @@
--- ETAPE 1
--- Création de subqueries pour chaque tables causes de décès afin de créer des clés unique pour réaliser les jointures
--- Concatenation du nom du pays "Country" + "-" + de l'année "Year" similaire sur toutes les tables
-
--- ETAPE 2
--- Jointure des tables sur la plus grande THH (environ 500 lignes), grâce à un INNER JOIN
--- Attention : parti pris de faire le INNER pour une analyse plus homogène de nos résultats sur la même temporalité (à savoir 2011 à 2021)
-
--- ETAPE 3
--- Calcul du SDR European Standardised Death Rate = ROUND((THH.value/100000),4)*100 as death_rate_THH pour chaque cause de décès
-
 WITH Sub1 AS (
 
     SELECT
     *, 
     CONCAT (Country, "_", Year) as Key
-    FROM {{ref("stg_raw__Heart")}} 
+    FROM {{ref("stg_raw__Heart")}}
     )
 
 , Sub2 AS (
@@ -58,16 +47,14 @@ WITH Sub1 AS (
     )
 
 SELECT 
-
 THH.Country,
 THH.Year,
-ROUND((THH.value/100000),4)*100 as death_rate_THH,
-ROUND((Heart.value/100000),4)*100 as death_rate_heart,
-ROUND((Cancer.value/100000),4)*100 as death_rate_cancer,
-ROUND((Liver.value/100000),4)*100 as death_rate_liver,
-ROUND((Nervous.value/100000),4)*100 as death_rate_nervous,
-ROUND((Pneumonia.value/100000),4)*100 as death_rate_pneumonia
-
+THH.value as death_rate_THH,
+Heart.value as death_rate_heart,
+Cancer.value as death_rate_cancer,
+Liver.value as death_rate_liver,
+Nervous.value as death_rate_nervous,
+Pneumonia.value as death_rate_pneumonia
 FROM Sub2 as THH 
 INNER JOIN Sub1 as Heart
 ON THH.Key = Heart.Key 
@@ -83,4 +70,3 @@ ON Nervous.Key = THH.Key
 
 INNER JOIN Sub6 as Pneumonia
 ON Pneumonia.Key = THH.Key
-
