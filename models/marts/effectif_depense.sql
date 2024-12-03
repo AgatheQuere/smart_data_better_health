875631e96088a0b3cb1e4dd930dbba368a83f261
Adrian Briot <adrian.briot@gmail.com>
2024-12-03 14:32:44 +0000

    ajout d'une colonne

--- models/marts/effectif_depense.sql
+++ models/marts/effectif_depense.sql
@@ -1,27 +1,36 @@
 SELECT 
-e.annee,
-e.patho_niv1,
-e.patho_niv2,
-e.patho_niv3,
-e.region,
-e.dept,
-e.region_name,
-e.region_iso,
-e.departement_iso,
-e.libelle_classe_age,
-e.cla_age_5,
-e.libelle_sexe,
-
-ROUND(AVG(e.prev),2) AS prevalence,
-SUM(e.Ntop) AS nombre_pris_en_charge,
-SUM(e.Npop) AS population_susceptible,
-AVG(d.montant_moy) AS montant_moyen,
-SUM(ROUND(e.Ntop*d.montant_moy,0)) AS montant_total
-
-
-FROM smart-data-le-wagon.dbt_lcazetou_intermediate.effectif_pathologie_code_iso e
-LEFT JOIN {{ ref('dépense_tout_patho') }} d USING (top)
-
-GROUP BY annee, patho_niv1, patho_niv2, patho_niv3, region, dept, region_name, region_iso, departement_iso, libelle_classe_age, cla_age_5, libelle_sexe
-
-
+  e.annee,
+  DATE(CAST(e.annee AS STRING) || '-01-01') as annee_format,
+  e.patho_niv1,
+  e.patho_niv2,
+  e.patho_niv3,
+  e.region,
+  e.dept,
+  e.region_name,
+  e.region_iso,
+  e.departement_iso,
+  e.libelle_classe_age,
+  e.cla_age_5,
+  e.libelle_sexe,
+  ROUND(100 * SUM(e.Ntop) / SUM(e.Npop), 2) AS prevalence,
+  SUM(e.Ntop) AS nombre_pris_en_charge,
+  SUM(e.Npop) AS population_susceptible,
+  AVG(d.montant_moy) AS montant_moyen,
+  SUM(ROUND(e.Ntop * d.montant_moy, 0)) AS montant_total
+FROM 
+  smart-data-le-wagon.dbt_lcazetou_intermediate.effectif_pathologie_code_iso e
+LEFT JOIN 
+  {{ ref('dépense_tout_patho') }} d ON e.top = d.top
+GROUP BY 
+  e.annee, 
+  e.patho_niv1, 
+  e.patho_niv2, 
+  e.patho_niv3, 
+  e.region, 
+  e.dept, 
+  e.region_name, 
+  e.region_iso, 
+  e.departement_iso, 
+  e.libelle_classe_age, 
+  e.cla_age_5, 
+  e.libelle_sexe
